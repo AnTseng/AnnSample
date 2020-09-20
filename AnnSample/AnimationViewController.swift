3558b6fe2107bc2bc73a2b6df2b1bee0debea821
Ann Tseng <ann_tseng@cmoney.com.tw>
2020-09-20 17:51:43 +0800

    style: add comment

--- AnnSample/AnimationViewController.swift
+++ AnnSample/AnimationViewController.swift
@@ -68,6 +68,7 @@ class AnimationViewController: UIViewController {
     }
     
     @objc private func cGAffineTransform() {
+        // 參考: https://medium.com/%E5%BD%BC%E5%BE%97%E6%BD%98%E7%9A%84-swift-ios-app-%E9%96%8B%E7%99%BC%E5%95%8F%E9%A1%8C%E8%A7%A3%E7%AD%94%E9%9B%86/83-%E5%88%A9%E7%94%A8-cgaffinetransform-%E7%B8%AE%E6%94%BE-%E4%BD%8D%E7%A7%BB%E5%92%8C%E6%97%8B%E8%BD%89-e061df9ed672
         animationView.transform = CGAffineTransform(scaleX: 2, y: 2)
     }
 }
